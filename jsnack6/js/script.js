39cffd37465fd815417a4012819c526c09aced62
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-11 17:35:59 +0100

    jsnack con uso di ES6 ok

--- jsnack6/js/script.js
+++ jsnack6/js/script.js
@@ -1,31 +1,47 @@
 /*Dato questo array di obj iniziale: var arrayObj = [{name: 'Poppy', type: 'tshirt', color: 'red'}, {name: 'Jumping', type: 'occhiali', color: 'blue'}, {name: 'CrissCross', type: 'scarpe', color: 'black'}, {name: 'Jenny', type: 'borsa', color: 'pink'},];
 Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array) una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale*/
 
-//mi creo un array di partenza
-var abbigliamento = [
+//mi creo un array di oggetto di partenza
+const abbigliamento = [
   {name: 'Poppy', type: 'tshirt', color: 'red'},
   {name: 'Jumping', type: 'occhiali', color: 'blue'},
   {name: 'CrissCross', type: 'scarpe', color: 'black'},
   {name: 'Jenny', type: 'borsa', color: 'pink'},
 ];
+console.table(abbigliamento);
 
-//mi creo una copia dell'array di partenza in cui andrò ad aggiungere una nuova proprietà dal nome position
-var abbigliamentoDue = [
-    {name: 'Poppy', type: 'tshirt', color: 'red'},
-    {name: 'Jumping', type: 'occhiali', color: 'blue'},
-    {name: 'CrissCross', type: 'scarpe', color: 'black'},
-    {name: 'Jenny', type: 'borsa', color: 'pink'},
-  ];
+//mi creo una copia dell'array di partenza nel quale aggiungerò la nuova proprietà position
+const abbigliamentoDue = [...abbigliamento];
 
-//con il ciclo for aggiungo la proprietà position con la variabile lettera
-for (var i = 0; i < abbigliamentoDue.length; i++){
-    var lettera = String.fromCharCode(97+Math.floor(Math.random() * 26)); ////mi genero delle lettere casuali minuscole
-    //var lettera = String.fromCharCode(65+Math.floor(Math.random() * 26)); //lettere maiuscole
-    abbigliamentoDue[i].position = lettera;
-}
- 
-console.log("Array con l'aggiunta di position: " , abbigliamentoDue);
+//con map aggiungo la proprietà position con un lettera random
+abbigliamentoDue.map((element) => {
+  element.position = String.fromCharCode(97+Math.floor(Math.random() * 26)); //mi genero delle lettere casuali minuscole
+});
+console.table(abbigliamentoDue);
 
+
+/*************ESERCIZIO SENZA L'USO DI ES6**********************/
+// //mi creo un array di partenza
+// var abbigliamento = [
+//   {name: 'Poppy', type: 'tshirt', color: 'red'},
+//   {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//   {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//   {name: 'Jenny', type: 'borsa', color: 'pink'},
+// ];
+// //mi creo una copia dell'array di partenza in cui andrò ad aggiungere una nuova proprietà dal nome position
+// var abbigliamentoDue = [
+//     {name: 'Poppy', type: 'tshirt', color: 'red'},
+//     {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//     {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//     {name: 'Jenny', type: 'borsa', color: 'pink'},
+//   ];
+// //con il ciclo for aggiungo la proprietà position con la variabile lettera
+// for (var i = 0; i < abbigliamentoDue.length; i++){
+//     var lettera = String.fromCharCode(97+Math.floor(Math.random() * 26)); //mi genero delle lettere casuali minuscole
+//     //var lettera = String.fromCharCode(65+Math.floor(Math.random() * 26)); //lettere maiuscole
+//     abbigliamentoDue[i].position = lettera;
+// }
+// console.log("Array con l'aggiunta di position: " , abbigliamentoDue);
 
 //metodo alternativo per generare delle lettere casuali utilizzando una funzione
 /*function lettereRandom() {
