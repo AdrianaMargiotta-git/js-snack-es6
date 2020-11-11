7b2c4ea3efdc8af6a5e9357440aae534daec9439
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-11 16:49:49 +0100

    jsnack con uso di ES6 ok

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -2,7 +2,8 @@
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti*/
 
-var squadre = [ 
+//creo l'array di oggetti
+const squadre = [ 
     {
         'nome' : 'Milan',
         'punti' : 0,
@@ -27,23 +28,55 @@ var squadre = [
 
 console.log(squadre);
 
+//applico il ciclo forEach sull'array di oggetti
+squadre.forEach((element) => {
+    element.punti = Math.floor(Math.random()*10);;
+    element.falli = Math.floor(Math.random()*10);;
+    //console.log(element.nome + ' ha totalizzato ' + element.punti + ' punti e ha subito ' + element.falli + ' falli'); //metodo classico
+    console.log(`
+    ${element.nome} ha totalizzato ${element.punti} punti e ha subito ${element.falli} falli
+    
+    `);
+});
 
+
+
+/*************ESERCIZIO SENZA L'USO DI ES6**********************/
+/*var squadre = [ 
+    {
+        'nome' : 'Milan',
+        'punti' : 0,
+        'falli' : 0
+    },
+    {
+        'nome' : 'Napoli',
+        'punti' : 0,
+        'falli' : 0
+    },
+    {
+        'nome' : 'Sampdoria',
+        'punti' : 0,
+        'falli' : 0
+    },
+    {
+        'nome' : 'Palermo',
+        'punti' : 0,
+        'falli' : 0
+    }
+];
+console.log(squadre);*/
 /***************FUNZIONI**********************/
-
-function random_punti(){
+/*function random_punti(){
     var numPunti = Math.floor(Math.random()*10);
     return numPunti;
 };
-
 function random_falli(){
     var numFalli = Math.floor(Math.random()*10);
     return numFalli;
-};
-
-/******************************************* */
-
-for (var i = 0; i < squadre.length; i++) {
+};*/
+/********************************************/
+/*for (var i = 0; i < squadre.length; i++) {
     squadre[i].punti = random_punti();
     squadre[i].falli = random_falli();
     console.log(squadre[i].nome + ' ha totalizzato ' + squadre[i].punti + ' punti e ha subito ' + squadre[i].falli + ' falli');
-}
+}*/
